8c0d8f720192e8d78ed99a4fa91673a1bf50703a
Ramon Xavier <ramon1234rxm@gmail.com>
2023-04-17 09:41:39 -0300

    feat: adding promises and filtered archives

--- projects/count-legends/src/readDir.js
+++ projects/count-legends/src/readDir.js
@@ -1,11 +1,29 @@
 const fs = require("node:fs");
 const path = require("node:path");
 
+const { log, error } = console;
+
 function readDir(filepath) {
-  const archives = fs.readdirSync(filepath);
-  console.log(archives);
+  return new Promise((resolve, reject) => {
+    try {
+      const archives = fs.readdirSync(filepath);
+      const mappedArchives = archives.map((archive) =>
+        path.join(filepath, archive)
+      );
+      resolve(mappedArchives);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+function elementsFinishedWith(array, pattern) {
+  return array.filter((el) => el.endsWith(pattern));
 }
 
 const filepath = path.join(__dirname, "..", "legends");
 
-readDir(filepath);
+readDir(filepath)
+  .then((archives) => elementsFinishedWith(archives, ".srt"))
+  .then(log)
+  .catch(error);
